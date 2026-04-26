a4f294fac2fcb90aa7d2570ba67b91e5016b4b87
rajatsubhrakar <rajatsubhrakar@gmail.com>
2026-04-25 20:31:10 -0500

    Model evaluation and model pusher components are modified to use the new AWS S3 storage for saving the trained model and evaluation reports. The configuration for AWS connection is added, and the cloud storage module is implemented to handle interactions with AWS S3. Additionally, the HTML templates and CSS styles are updated for better user interface.

--- mlopsdl/constants/__init__.py
+++ mlopsdl/constants/__init__.py
@@ -24,8 +24,8 @@ SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 
 
-AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
-AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+AWS_ACCESS_KEY_ID_ENV_KEY = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY_ENV_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 REGION_NAME = "us-east-1"
 
 DATA_INGESTION_COLLECTION_NAME: str = "easyvisa_collection"
@@ -66,7 +66,7 @@ MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 MODEL EVALUATION related constant 
 """
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-MODEL_BUCKET_NAME = "usvisa-model2024"
+MODEL_BUCKET_NAME = "mlopsdl-model2024"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
 
